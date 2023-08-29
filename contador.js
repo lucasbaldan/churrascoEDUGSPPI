1cc71ab08c03872665ed334a50f59fde343b314b
Lucas Faé Baldan <105757358+lucasbaldan@users.noreply.github.com>
2023-08-29 13:49:27 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const targetDate = new Date("2023-12-02T09:00:00").getTime();
+
+    function updateCounter() {
+        const currentDate = new Date().getTime();
+        const timeRemaining = targetDate - currentDate;
+
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
+    }
+
+    updateCounter();
+    setInterval(updateCounter, 1000);
+});
